ddd9836795bd6c7fdeb2abd45218100d0fe0d08a
priyank <21bmiit145@gmail.com>
2023-04-09 14:27:21 +0530

    Login Form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Invitation_Project
 {
-    partial class Form1
+    partial class loginForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.lbl_err = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(79, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 94);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Brush Script MT", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(25, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Brush Script MT", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(25, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_submit.Font = new System.Drawing.Font("Daddy Rewind", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.btn_submit.Location = new System.Drawing.Point(102, 293);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(152, 45);
+            this.btn_submit.TabIndex = 3;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = false;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackColor = System.Drawing.SystemColors.Control;
+            this.txt_username.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt_username.Location = new System.Drawing.Point(152, 158);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(176, 36);
+            this.txt_username.TabIndex = 4;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.BackColor = System.Drawing.SystemColors.Control;
+            this.txt_pass.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt_pass.Location = new System.Drawing.Point(152, 223);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(176, 36);
+            this.txt_pass.TabIndex = 5;
+            // 
+            // lbl_err
+            // 
+            this.lbl_err.AutoSize = true;
+            this.lbl_err.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_err.ForeColor = System.Drawing.Color.Firebrick;
+            this.lbl_err.Location = new System.Drawing.Point(92, 352);
+            this.lbl_err.Name = "lbl_err";
+            this.lbl_err.Size = new System.Drawing.Size(0, 18);
+            this.lbl_err.TabIndex = 6;
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(362, 388);
+            this.Controls.Add(this.lbl_err);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "loginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.loginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Button btn_submit;
+        private TextBox txt_username;
+        private TextBox txt_pass;
+        private Label lbl_err;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,48 @@
+using DotNetEnv;
+using System.Data.SqlClient;
+
 namespace Invitation_Project
 {
-    public partial class Form1 : Form
+    public partial class loginForm : Form
     {
-        public Form1()
+        //String con_str = Environment.GetEnvironmentVariable("con_string");
+        SqlCommand cmd;
+        SqlConnection con = new SqlConnection("Data Source=SPIRIT-SOLUTION\\SQLEXPRESS;Initial Catalog=Invitation_Project;Integrated Security=True");
+        SqlDataReader dr;
+     
+        public loginForm()
         {
+            Env.Load(); 
             InitializeComponent();
+        }
+        
+        private void loginForm_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("D:\\UTU\\GUI_1\\Invitation_Project\\picture\\system_pic\\1.png");
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            con.Open();
+            cmd = new SqlCommand("select * from tbl_auth where username = @username and password = @psd;" , con);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@username", txt_username.Text);
+            cmd.Parameters.AddWithValue("@psd" , txt_pass.Text);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            if (dr.HasRows)
+            {
+                dashboard d = new dashboard();
+                this.DestroyHandle();
+                d.ShowDialog();
+            }
+            else
+            {
+                lbl_err.Text = "Not Valid credentials ";
+            }
+            con.Close() ;
+
         }
     }
 }
